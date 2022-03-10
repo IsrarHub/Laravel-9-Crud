f7b6722bdc7f5780248c5bc9482c8c1caab29138
IsrarHub <57617709+IsrarHub@users.noreply.github.com>
2022-03-10 17:15:28 +0500

    cehc

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,10 @@
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   @stack('css')
+  
+<link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+<!-- Theme style -->
+<link rel="stylesheet" href="dist/css/adminlte.min.css">
   <!-- Font Awesome -->
 </head>
 <body class="hold-transition sidebar-mini">
